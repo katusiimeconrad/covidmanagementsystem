d5873d18e2c5314ddbb9f4692ad9825978195677
ssembatyaisaac <ssembatyaisaac1999@gmail.com>
2021-01-27 12:27:02 +0300

    Added include

---
+++ resources/views/template/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2020 <a href="https://adminlte.io">COVID-19 Team</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0
+    </div>
+  </footer>
